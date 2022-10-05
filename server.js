8122d4ca0526f6db9fa20b2bffc0be0dc110af48
✧Victor✧ <68920741+vkrenz@users.noreply.github.com>
2022-10-05 15:24:57 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+const port = 8080
+
+app.set('view engine', 'ejs')
+
+app.get('/', (req, res) => {
+    res.render('index', {})
+})
+
+app.listen(8080)
+
